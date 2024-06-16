956bec50e9d43ff1a22f24a5257be8586aa74647
aMarkusa <makke00694@gmail.com>
2024-06-16 21:33:50 +0300

    fixing failed commit

--- app_iostream_usart.c
+++ app_iostream_usart.c
@@ -1,20 +1,20 @@
 /***************************************************************************/ /**
-                                                                               * @file
-                                                                               * @brief iostream usart examples functions
-                                                                               *******************************************************************************
-                                                                               * # License
-                                                                               * <b>Copyright 2020 Silicon Laboratories Inc. www.silabs.com</b>
-                                                                               *******************************************************************************
-                                                                               *
-                                                                               * The licensor of this software is Silicon Laboratories Inc. Your use of this
-                                                                               * software is governed by the terms of Silicon Labs Master Software License
-                                                                               * Agreement (MSLA) available at
-                                                                               * www.silabs.com/about-us/legal/master-software-license-agreement. This
-                                                                               * software is distributed to you in Source Code format and is governed by the
-                                                                               * sections of the MSLA applicable to Source Code.
-                                                                               *
-                                                                               * TODO: Think about error handling
-                                                                               ******************************************************************************/
+* @file
+* @brief iostream usart examples functions TODO: Change name of the file and add descriptions
+*******************************************************************************
+* # License
+* <b>Copyright 2020 Silicon Laboratories Inc. www.silabs.com</b>
+*******************************************************************************
+*
+* The licensor of this software is Silicon Laboratories Inc. Your use of this
+* software is governed by the terms of Silicon Labs Master Software License
+* Agreement (MSLA) available at
+* www.silabs.com/about-us/legal/master-software-license-agreement. This
+* software is distributed to you in Source Code format and is governed by the
+* sections of the MSLA applicable to Source Code.
+*
+* TODO: Think about error handling
+******************************************************************************/
 #include "app_iostream_usart.h"
 #include "app_fl.h"
 #include "em_chip.h"
